4449ef4c2eaf6692a6d3cd307228a2eae4c170b1
Adam Rawlins <agrawlins@gmail.com>
2022-05-18 21:34:49 -0400

    Completed functionality and styled to allow for maximum utility of the page. Overall success today, and project complete!

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,24 +1,24 @@
 import React from "react"
 const Name = () => {
-    const [badgeArray, setbadgeArray] = React.useState([""])
+    const [badgeArray, setbadgeArray] = React.useState([])
     
     // const badgeElements = badgeArray.map((name, i) => <p key={i}>{name}</p>)
     const badgeElements = badgeArray.map((badge, i) => {
+        const a = badge.phone
+        const phoneFormat = `(${a[0]}${a[1]}${a[2]}) ${a[3]}${a[4]}${a[5]}- ${a[6]}${a[7]}${a[8]}${a[9]}`
         // if (typeof name === 'string') return (<p key={i}>{name}</p>)
         if (typeof badge === 'object') return (<div key={i}>
             <label><h1>{badge.firstName}'s Badge:</h1></label>
             <p>Name: {badge.firstName + ' ' + badge.lastName}</p>
             <p>Email: {badge.email}</p>
-            <p>Phone#:</p>
+            <p>Phone#: {phoneFormat}</p>
             <p>Place of Birth: {badge.placeOfBirth}</p>
             <p>Favorite Food: {badge.food}</p>
             <p>About You: {badge.comments}</p>
             </div>)
-})
+    })
     
-  
-
-    const [formData, setFormData] = React.useState({
+    const intState = {
         firstName: "",
         lastName: "",
         email: "",
@@ -26,7 +26,9 @@ const Name = () => {
         placeOfBirth: "",
         food: "",
         comments: ""
-    })
+    }
+
+    const [formData, setFormData] = React.useState(intState)
 
     const handleChange = (event) => {
         const {name, value, type} = event.target
@@ -42,6 +44,15 @@ const Name = () => {
         setbadgeArray(prevName => {
             return [...prevName, formData]
         })
+        setFormData(intState)
+    }
+
+    const SubmitButton = () => {
+        if (!formData.firstName || !formData.lastName || !formData.email || !formData.phone || !formData.placeOfBirth || !formData.food || !formData.comments) {
+            return <button className="form--submit-disabled"disabled>Submit</button>
+        } else {
+            return <button className="form--submit">Submit</button>
+        }
     }
     
     return (
@@ -56,6 +67,7 @@ const Name = () => {
                         name="firstName"
                         onChange={handleChange}
                         value={formData.firstName}
+                        minLength={3}
                         required                      
                       />
                     <input 
@@ -65,6 +77,7 @@ const Name = () => {
                         name="lastName"
                         onChange={handleChange}
                         value={formData.lastName}
+                        minLength={3}
                         required                      
                       />
                     <input 
@@ -74,16 +87,18 @@ const Name = () => {
                         name="email"
                         onChange={handleChange}
                         value={formData.email}
+                        minLength={3}
                         required                    
                     />
                     <input 
                         type="tel"
-                        pattern="[0-9]{10}"
+                        pattern="([0-9]{10})"
                         placeholder="Phone#"
                         className="form--input"
                         name="phone"
                         onChange={handleChange}
                         value={formData.phone}
+                        minLength={3}
                         required                    
                     />
                     <input 
@@ -93,6 +108,7 @@ const Name = () => {
                         name="placeOfBirth"
                         onChange={handleChange}
                         value={formData.placeOfBirth}
+                        minLength={3}
                         required                    
                     />
                      <input 
@@ -102,18 +118,20 @@ const Name = () => {
                         name="food"
                         onChange={handleChange}
                         value={formData.food}
+                        minLength={3}
                         required                    
                     />
-                    <textarea
+                    <input
                         type="text" 
                         placeholder="Tell us about yourself"
-                        className="form--textarea"
+                        className="form--comments"
                         name="comments"
                         onChange={handleChange}
                         value={formData.comments}
+                        minLength={3}
                         required                    
                     />
-                    <button className="form--submit">Submit</button>
+                    <SubmitButton />
                 </form>
                 <br />
                 <div className="list">
